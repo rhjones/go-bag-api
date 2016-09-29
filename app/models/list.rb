d92cb5b47828ea09655d25507157fa8048c7fe40
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-29 12:12:55 -0400

    Add validation to List model
    Require title and user

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ActiveRecord::Base
+  belongs_to :user
+  validates :title, :user, presence: true
   has_many :items, through: :contents
   has_many :contents, dependent: :destroy
-  belongs_to :user
 end
