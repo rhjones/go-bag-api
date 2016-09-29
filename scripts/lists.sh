adb728e241240b9cd481a7ae660d9499420bf0f8
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-29 12:13:44 -0400

    Update curl scripts for lists with token
    Add auth token to curl scripts for lists

--- scripts/lists.sh
+++ scripts/lists.sh
@@ -1,28 +1,32 @@
 # create
 curl --include --request POST http://localhost:3000/lists \
   --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN" \
   --data '{
     "list": {
-      "title": "White Mountains May 2016",
-      "user_id": 1
+      "title": "White Mountains Sept 2016"
     }
   }'
 
 # update
-curl --include --request PATCH http://localhost:3000/lists/3 \
+curl --include --request PATCH http://localhost:3000/lists/$ID \
   --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN" \
   --data '{
     "list": {
-      "title": "Maine Camping June 2016",
-      "user_id": 1
+      "title": "Maine Camping June 2016"
     }
   }'
 
 #index
-curl --include --request GET http://localhost:3000/lists
+curl --include --request GET http://localhost:3000/lists \
+  --header "Authorization: Token token=$TOKEN"
 
 #show
-curl --include --request GET http://localhost:3000/lists/2
+curl --include --request GET http://localhost:3000/lists/$ID \
+  --header "Authorization: Token token=$TOKEN"
+
 
 #delete
-curl --include --request DELETE http://localhost:3000/lists/1
+curl --include --request DELETE http://localhost:3000/lists/$ID \
+  --header "Authorization: Token token=$TOKEN"
