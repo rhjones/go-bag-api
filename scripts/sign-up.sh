be97e84b2cf8091e617448ab0b161944f077ced7
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-29 12:14:36 -0400

    Change data in curl scripts for sign up and in

--- scripts/sign-up.sh
+++ scripts/sign-up.sh
@@ -14,8 +14,8 @@ curl --include --request POST http://localhost:3000/sign-up \
   --header "Content-Type: application/json" \
   --data '{
     "credentials": {
-      "email": "another@example.email",
-      "password": "an example password",
-      "password_confirmation": "an example password"
+      "email": "test@example.email",
+      "password": "pwd",
+      "password_confirmation": "pwd"
     }
   }'
