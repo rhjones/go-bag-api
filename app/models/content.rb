3a4fe52c2d4df08b07d53805e96929f0206a4bed
rebekahheacock <rebekah.heacock@gmail.com>
2016-10-01 19:24:21 -0400

    Add uniqueness constraints to Content and Item
    Items must have unique names
    Content must be unique on a list (already existed, just refactoring here)

--- app/models/content.rb
+++ app/models/content.rb
@@ -5,5 +5,5 @@ class Content < ActiveRecord::Base
 
   belongs_to :item, inverse_of: :contents
   belongs_to :list, inverse_of: :contents
-  validates_uniqueness_of :item_id, scope: :list_id
+  validates :item_id, uniqueness: { scope: :list_id }
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ActiveRecord::Base
   has_many :lists, through: :contents
   has_many :contents, dependent: :destroy
+  validates :name, uniqueness: :true
 
   def self.search(query)
     where('name LIKE ?', "%#{query}%")
