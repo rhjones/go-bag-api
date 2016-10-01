a72be360b86c21a1ad4bfb0ccf7b2d3351d07f49
rebekahheacock <rebekah.heacock@gmail.com>
2016-10-01 18:57:52 -0400

    Set default_scope on Content model
    By default, contents should order from the most recently updated to the least recently updated
    Set a default_scope in the model

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,8 @@
 class Content < ActiveRecord::Base
+  def self.default_scope
+    order('updated_at DESC')
+  end
+
   belongs_to :item, inverse_of: :contents
   belongs_to :list, inverse_of: :contents
   validates_uniqueness_of :item_id, scope: :list_id
