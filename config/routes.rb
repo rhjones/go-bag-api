3aa6c5a8b8d110cb884a863a65b0dd83364b7e41
rebekahheacock <rebekah.heacock@gmail.com>
2016-10-02 11:04:56 -0400

    Remove comments from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :contents, except: [:new, :edit, :index, :show]
-  # resources :contents, except: [:new, :edit, :index]
-  # get '/contents/list/:list_id' => 'contents#index_for_list'
   resources :items, except: [:new, :edit]
   resources :lists, except: [:new, :edit]
   resources :examples, except: [:new, :edit]
