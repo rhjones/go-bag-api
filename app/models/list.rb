dfebd4dc617d7892455a0775f97af3a9a5a495b3
rebekahheacock <rebekah.heacock@gmail.com>
2016-10-02 13:19:44 -0400

    Add unique requirement for list titles for single user
    The titles of a user's lists must all be unique.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ActiveRecord::Base
   belongs_to :user
   validates :title, :user, presence: true
+  validates :title, uniqueness: { scope: :user_id }
   has_many :items, through: :contents
   has_many :contents, dependent: :destroy
 end
